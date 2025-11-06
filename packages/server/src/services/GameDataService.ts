18ebd41bc23cea68118d1d9100e47d0dac0caac0
f2re <f2re@yandex.ru>
2025-11-06 13:42:56 +0300

    fix shared

--- packages/server/src/services/GameDataService.ts
+++ packages/server/src/services/GameDataService.ts
@@ -1,4 +1,5 @@
 import { GameDataModel, GameDataRow } from '../models/GameData';
+import { GameStatesModel, GameStatePayload } from '../models/GameStates';
 import { GameData as SharedGameData } from '@shared/types';
 
 export class GameDataService {
@@ -16,5 +17,22 @@ export class GameDataService {
 
   static async deleteGameDataByUserId(userId: string): Promise<boolean> {
     return await GameDataModel.deleteByUserId(userId);
+  }
+
+  // Game state methods
+  static async saveGameState(userId: string, gameState: GameStatePayload): Promise<void> {
+    await GameStatesModel.saveGameState(userId, gameState);
+  }
+
+  static async getGameState(userId: string): Promise<any> {
+    return await GameStatesModel.getGameState(userId);
+  }
+
+  static async updateGameState(userId: string, gameState: Partial<GameStatePayload>): Promise<void> {
+    await GameStatesModel.updateGameState(userId, gameState);
+  }
+
+  static async deleteGameState(userId: string): Promise<boolean> {
+    return await GameStatesModel.deleteGameState(userId);
   }
 }
